194dd4942147d879cbda1c418eb5e99e1c7e1636
vetonshabani0 <68129513+vetonshabani0@users.noreply.github.com>
2021-06-25 17:44:43 +0200

    Created product filtering

--- MyOwnShop/MyOwnShop.WebUI/Controllers/HomeController.cs
+++ MyOwnShop/MyOwnShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyOwnShop.Core.Contracts;
 using MyOwnShop.Core.Models;
+using MyOwnShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,10 +18,24 @@ namespace MyOwnShop.WebUI.Controllers
 			context = productContext;
 			productCategories = productCategoryContext;
 		}
-		public ActionResult Index()
+		public ActionResult Index(string Category=null)
 		{
-			List<Product> products = context.Collection().ToList();
-			return View(products);
+			List<Product> products;
+			List<ProductCategory> categories= productCategories.Collection().ToList();
+
+			if (Category == null)
+			{
+				products=context.Collection().ToList();
+			}
+			else {
+				products = context.Collection().Where(p => p.Category == Category).ToList();
+			}
+			ProductListViewModel model = new ProductListViewModel();
+			model.Products = products;
+			model.ProductCategories = categories;
+
+
+			return View(model);
 		}
 
 		public ActionResult Details(string Id)
